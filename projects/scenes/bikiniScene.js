706ca38b05a934b7a52e5fda8952b5b4e097fcfb
slpys <edwinjuarbej@gmail.com>
2024-12-10 02:55:02 -0500

    fixed pathing?

--- projects/scenes/bikiniScene.js
+++ projects/scenes/bikiniScene.js
@@ -3,7 +3,7 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155.0/build/three.m
 export function bikiniScene(scene) {
     // Blue Background (Underwater Effect)
 
-    loader.load('./flowersky.jpg', function(texture) {
+    loader.load('../images/flowersky.jpg', function(texture) {
         scene.background = texture; // Set the image as the background
     });
 
